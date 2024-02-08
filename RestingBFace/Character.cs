02aa2adda934eef36af8745b0bbed0e1713fa9a0
Hugo Zingmark <hugo.priebezingmark2@elev.ga.ntig.se>
2024-02-08 17:10:36 +0100

    40 grader feber

---
+++ RestingBFace/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+public class Character
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("gender")]
+    public string Gender { get; set; }
+
+    [JsonPropertyName("height")]
+    public int Height { get; set; }
+}
---
+++ RestingBFace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using RestSharp;
+using System.IO;
+using System.Runtime.InteropServices;
+
+RestClient client = new("https://swapi.py4e.com/api/");
+
+Console.WriteLine("Which character?");
+string charName = Console.ReadLine();
+RestRequest request = new($"people/{charName}/");
+
+RestResponse response = client.GetAsync(request).Result;
+
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    Character c = JsonSerializer.Deserialize<Character>(response.Content);
+    Console.WriteLine(c.Height);
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+//Console.WriteLine(response.Content);
+//File.WriteAllText("StarWars.json", response.Content);
+
+Console.ReadLine();
